fb8d194697381d2b4d40a18ffbcd6a3a52a3973a
gary\user <garybird606@gmail.com>
2021-10-06 15:04:35 +0800

    Delete Route method

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -21,24 +21,24 @@ export default class InputContent extends Component {
 
     switch (route) {
       case 'record':
-        return <Route path='/'><div>
+        return <div>
             <Link to='/account/total'><img style={styles.costImg} alt="" onClick={()=>this.props.changePage('total')}  src={require('../../assets/img/cost-list.png')}/></Link>
             <Link to='/account/chart'><img style={{...styles.chartImg, zIndex: 5}} alt="" onClick={()=>this.props.changePage('chart')} src={require('../../assets/img/chart.png')}/></Link>
-            </div></Route>;
+            </div>;
       case 'total':
-        return <Route path='/'><div>
+        return <div>
             <Link to='/account/'><img style={styles.costImg} alt="" onClick={()=>this.props.changePage('record')} src={require('../../assets/img/record.png')}/></Link>
-          </div></Route>;
+          </div>;
       case 'detailOfMonth':
-        return <Route path='/'><div>
+        return <div>
             <Link to='/account/total'><img style={styles.costImg} alt="" onClick={()=>this.props.changePage('total')}  src={require('../../assets/img/cost-list.png')}/></Link>
             <Link to='/account/'><img style={{...styles.recordImg, zIndex: 5}} alt="" onClick={()=>this.props.changePage('record')} src={require('../../assets/img/record.png')}/></Link>
-          </div></Route>;
+          </div>;
       case 'chart':
-        return <Route path='/'><div>
+        return <div>
             <Link to='/account/total'><img style={styles.costImg} alt="" onClick={()=>this.props.changePage('total')}  src={require('../../assets/img/cost-list.png')}/></Link>
             <Link to='/account/'><img style={{...styles.recordImg, zIndex: 5}} alt="" onClick={()=>this.props.changePage('record')} src={require('../../assets/img/record.png')}/></Link>
-          </div></Route>;
+          </div>;
       default:
         return
     }
@@ -53,7 +53,7 @@ export default class InputContent extends Component {
         <div style={styles.header}>
           {<div style={{position: 'relative'}}>
             {this.getImage(route)}
-            {<div style={{position: 'relative'}}><Route path='/'><Link to='/account/login'><img style={{...styles.logoutImg, right:'0px'}} alt="" onClick={()=>this.props.logOut()} src={require('../../assets/img/logout.png')}/></Link></Route>
+            {<div style={{position: 'relative'}}><Link to='/account/login'><img style={{...styles.logoutImg, right:'0px'}} alt="" onClick={()=>this.props.logOut()} src={require('../../assets/img/logout.png')}/></Link>
             </div>}
           </div>}
           <div style={styles.ctime}>現在時間: {currentTime}</div>
